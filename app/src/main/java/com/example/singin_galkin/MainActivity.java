9284f37ecbc6996dc0fa262d6a10d0d75e089fa0
Nikita <galkinnikita228@gmail.com>
2024-06-03 20:11:02 +0500

    Разработанный код для переключения страниц. Galkin

--- app/src/main/java/com/example/singin_galkin/MainActivity.java
+++ app/src/main/java/com/example/singin_galkin/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.singin_galkin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public int start_x = 0;
+
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        switch (event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                start_x = (int)event.getX();
+                break;
+            case  MotionEvent.ACTION_UP:
+                if(Math.abs((int)event.getX() - start_x) > 50)
+                {
+                    if(start_x < (int)event.getX())
+                    {
+                        setContentView((R.layout.activity_main));
+                    } else {
+                        setContentView(R.layout.regin);
+                    }
+                }
+        }
+        return false;
+    }
+
 }
