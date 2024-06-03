50a01d5fcbcacf0a85129c3fac043eaae974baad
Nikita <galkinnikita228@gmail.com>
2024-06-03 20:37:14 +0500

    Выполненная работа на оценку 5. Galkin

--- app/src/main/java/com/example/singin_galkin/MainActivity.java
+++ app/src/main/java/com/example/singin_galkin/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,27 +16,69 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public int start_x = 0;
+    private int startY;
+    private int currentPage;
 
     @Override
-    public boolean onTouchEvent(MotionEvent event)
-    {
-        switch (event.getAction())
-        {
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                start_x = (int)event.getX();
+                startY = (int) event.getY();
                 break;
-            case  MotionEvent.ACTION_UP:
-                if(Math.abs((int)event.getX() - start_x) > 50)
-                {
-                    if(start_x < (int)event.getX())
-                    {
-                        setContentView((R.layout.activity_main));
-                    } else {
-                        setContentView(R.layout.regin);
-                    }
+            case MotionEvent.ACTION_UP:
+                int endY = (int) event.getY();
+                if (startY - endY > 50) { // Свайп вверх
+                    goToNextPage();
+                } else if (endY - startY > 50) { // Свайп вниз
+                    goToPreviousPage();
                 }
+                break;
         }
-        return false;
+        return true;
     }
 
+    private void goToNextPage() {
+        switch (currentPage) {
+            case 0:
+                setContentView(R.layout.regin);
+                currentPage = 1;
+                break;
+            case 1:
+                setContentView(R.layout.password);
+                currentPage = 2;
+                break;
+        }
+    }
+
+    private void goToPreviousPage() {
+        switch (currentPage) {
+            case 1:
+                setContentView(R.layout.activity_main);
+                currentPage = 0;
+                break;
+            case 2:
+                setContentView(R.layout.regin);
+                currentPage = 1;
+                break;
+        }
+    }
+
+    public void onPassword(View view) {
+        setContentView(R.layout.password);
+    }
+
+    public void onStart(View view) {
+        EditText passwordEditText = findViewById(R.id.password);
+        EditText repeatPasswordEditText = findViewById(R.id.reapeatPassword);
+
+        String password = passwordEditText.getText().toString();
+        String repeatPassword = repeatPasswordEditText.getText().toString();
+
+        if (password.equals(repeatPassword)) {
+            setContentView(R.layout.activity_main);
+        } else {
+            Toast toast = Toast.makeText(this, "Пароли не совпадают!", Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
 }
